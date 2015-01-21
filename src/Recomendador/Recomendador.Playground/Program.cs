ebc3e4e97b3732d83d5a9edb136f3d55221d345c
Denis Ferrari <denisferrari@azys.com.br>
2015-01-21 21:58:39 -0200

    Criação da Solução com um Projeto Playground.

---
+++ src/Recomendador/Recomendador.Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recomendador.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Como obter os posts do Blog do Martin Fowler?
+            // Como eu envio um e-mail?
+            // Como salvar as recomendações em MongoDB?
+
+
+        }
+    }
+}
