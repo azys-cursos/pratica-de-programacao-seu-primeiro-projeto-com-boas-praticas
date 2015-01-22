a3dec2899287550bad3084a952cd640a5411e34b
Denis Ferrari <denisferrari@azys.com.br>
2015-01-21 23:38:08 -0200

    Obtendo os Posts do Blog do Martin Fowler e selecionando um artigo aleatório.

--- src/Recomendador/Recomendador.Playground/Program.cs
+++ src/Recomendador/Recomendador.Playground/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TNX.RssReader;
 
 namespace Recomendador.Playground
 {
@@ -11,6 +12,22 @@ namespace Recomendador.Playground
         static void Main(string[] args)
         {
             // Como obter os posts do Blog do Martin Fowler?
+            var urlFeedBlogMartinFowler = "http://martinfowler.com/feed.atom";
+
+            var feed = RssHelper.ReadFeed(urlFeedBlogMartinFowler);
+
+            var posts = feed.Items;
+
+            var artigos = posts.Where(p => p.Link.Contains("articles")).ToList();
+
+            var random = new Random();
+            var numeroAleatorio = random.Next(0, artigos.Count());
+            var artigoSelecionado = artigos[numeroAleatorio];
+
+            Console.WriteLine(artigoSelecionado.Title);
+
+            Console.ReadLine();
+
             // Como eu envio um e-mail?
             // Como salvar as recomendações em MongoDB?
 
